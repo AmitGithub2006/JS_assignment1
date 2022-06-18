7571ba09e0c3e5f02cbd855e4b5d8ad8e941e394
Amit Kumar <shakya94ak@gmail.com>
2022-06-18 19:46:11 +0530

    2nd ques added

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 //Ques.1:-Using a loop print all odd numbers up to and including n. Don’t include 0.
 
-let n = parseInt(process.argv[2]);
-for (let i = 1; i <= n; i++) {
-  if (i % 2 != 0) {
-    console.log(i);
+// let n = parseInt(process.argv[2]);
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+// }
+
+//Ques2:-Print the sum of all even numbers in a range from a to b. (Including a,b)
+
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[3]);
+let sum = 0;
+for (i = a; i <= b; i++) {
+  if (i % 2 == 0) {
+    sum += i;
   }
 }
+console.log(sum);
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 <img src="firstQues.png" alt="First Ques Image" />
+<img src="secondQues.png" alt="Second Ques Image" />
