39758319e1b9fc2014b3d7943414c155e1160b2b
Amit Kumar <shakya94ak@gmail.com>
2022-06-18 21:58:06 +0530

    3rd ques added

--- index.js
+++ index.js
@@ -9,12 +9,23 @@
 
 //Ques2:-Print the sum of all even numbers in a range from a to b. (Including a,b)
 
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
-let sum = 0;
-for (i = a; i <= b; i++) {
-  if (i % 2 == 0) {
-    sum += i;
-  }
+// let a = parseInt(process.argv[2]);
+// let b = parseInt(process.argv[3]);
+// let sum = 0;
+// for (i = a; i <= b; i++) {
+//   if (i % 2 == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+//Ques3:-Write a program to read the first and last digit of a number.
+
+let num = parseInt(process.argv[2]);
+let firstDigit = num;
+while (firstDigit >= 10) {
+  firstDigit = parseInt(firstDigit / 10);
 }
-console.log(sum);
+lastDigit = num % 10;
+
+console.log(firstDigit + "," + lastDigit);
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 <img src="firstQues.png" alt="First Ques Image" />
 <img src="secondQues.png" alt="Second Ques Image" />
+<img src="thirdQues.png" alt="Third Ques Image" />
