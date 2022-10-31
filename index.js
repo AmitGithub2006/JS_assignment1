dce4969c01476b17bc0c66bb53bf5052f9ce8a07
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2022-10-31 15:15:49 +0530

    ques

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Ques.1:-Using a loop print all odd numbers up to and including n. Don’t include 0.
 
-// let n = parseInt(process.argv[2]);
-// for (let i = 1; i <= n; i++) {
-//   if (i % 2 != 0) {
-//     console.log(i);
-//   }
-// }
+let n = parseInt(process.argv[2]);
+for (let i = 1; i <= n; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
 
 //Ques2:-Print the sum of all even numbers in a range from a to b. (Including a,b)
 
