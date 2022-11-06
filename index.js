69fce6a99f647150119b03fafe1a350d1f87daf7
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2022-11-06 22:01:51 +0530

    7th ques

--- index.js
+++ index.js
@@ -77,16 +77,16 @@ for (let i = 1; i <= n; i++) {
 
 //Ques7:-Check whether the given number is equal to its reverse number or not.
 
-// let num = parseInt(process.argv[2]);
-// let rev = 0;
-// let temp = num;
-// while (temp > 0) {
-//   last = temp % 10;
-//   rev = rev * 10 + last;
-//   temp = parseInt(temp / 10);
-// }
-// if (rev == num) {
-//   console.log("This number is equal to its reverse number");
-// } else {
-//   console.log("This number is not equal to its reverse number");
-// }
+let num = parseInt(process.argv[2]);
+let rev = 0;
+let temp = num;
+while (temp > 0) {
+  last = temp % 10;
+  rev = rev * 10 + last;
+  temp = parseInt(temp / 10);
+}
+if (rev == num) {
+  console.log("This number is equal to its reverse number");
+} else {
+  console.log("This number is not equal to its reverse number");
+}
